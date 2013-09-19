165b9c6d538bab8f39ca17482644f5b8628bb450
Dmitriy Dligach <dimdym@gmail.com>
2013-09-19 11:29:58 -0400

    firs version of conjunction miner

--- temporal/src/durations/ConjunctionMiner.java
+++ temporal/src/durations/ConjunctionMiner.java
@@ -3,8 +3,7 @@ package durations;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.CorruptIndexException;
@@ -20,12 +19,10 @@ import org.apache.lucene.store.FSDirectory;
 import utils.Utils;
 
 /**
- * Extract durations of sign/symptom(s).
- * 
- * TODO: what if a document contains multiple instances of a pattern? are all used?
+ * TODO: it seems like only the first occurence in a document of a query is returned.
+ * TODO: probably need to get all of them.
  */
-
-public class SignSymptomDurations {
+public class ConjunctionMiner {
 	
 	public static void main(String[] args) throws CorruptIndexException, IOException, ParseException, InvalidTokenOffsetsException {
 
@@ -33,36 +30,35 @@ public class SignSymptomDurations {
 		final String searchField = "content";
 		final String indexLocation = "/home/dima/data/mimic/index/";
 		final String signAndSymptomFile = "/home/dima/thyme/duration/data/unique-sign-symptoms.txt";
-		final String outputDirectory = "/home/dima/out/context/";
+		final String outputFile = "/home/dima/out/conjunction/counts.txt";
 		final int contextWindowInCharacters = 50;
-		final List<String> durationIndicators = Arrays.asList("for", "x");
 		
+		BufferedWriter writer = Utils.getWriter(outputFile, false);
     IndexReader indexReader = IndexReader.open(FSDirectory.open(new File(indexLocation)));
     IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 
-		for(String symptom : Utils.readSetValuesFromFile(signAndSymptomFile)) {
-      String outputFile = outputDirectory + symptom + ".txt";
-      BufferedWriter writer = Utils.getWriter(outputFile, false);
-      
-		  for(String durationIndicator : durationIndicators) {
-		    String queryText = symptom + " " + durationIndicator;
+    Set<String> signsAndSymptoms = Utils.readSetValuesFromFile(signAndSymptomFile);
+    
+		for(String ss1 : signsAndSymptoms) {
+		  for(String ss2 : signsAndSymptoms) {
+		    
+		    String queryText = ss1 + " and " + ss2;
 		    PhraseQuery phraseQuery = Utils.makePhraseQuery(queryText, searchField, 0);
-
+        
 		    TopDocs topDocs = indexSearcher.search(phraseQuery, maxHits);
-		    ScoreDoc[] scoreDocs = topDocs.scoreDocs;  		
+        ScoreDoc[] scoreDocs = topDocs.scoreDocs;     
 
-		    for(ScoreDoc scoreDoc : scoreDocs) {
-		      Document document = indexSearcher.doc(scoreDoc.doc);
-		      String text = document.get(searchField).toLowerCase().replace("[\n\r]", " ");      // TODO: lowercase or not?
-		      String context = Utils.getContext(queryText, text, contextWindowInCharacters);
-		      String contextWithNonPrintableCharactersRemoved = context.replaceAll("[^\\x20-\\x7E]", ""); // allowable range in hex
-		      writer.write(contextWithNonPrintableCharactersRemoved + "\n");
-		    }
+        for(ScoreDoc scoreDoc : scoreDocs) {
+          Document document = indexSearcher.doc(scoreDoc.doc);
+          String text = document.get(searchField).toLowerCase().replace("[\n\r]", " ");    
+          String context = Utils.getContext(queryText, text, contextWindowInCharacters);
+          String contextWithNonPrintableCharactersRemoved = context.replaceAll("[^\\x20-\\x7E]", "");
+          writer.write(queryText + ": " + contextWithNonPrintableCharactersRemoved + "\n");
+        }
 		  }
-		  
-      writer.close();
 		}
 		
+		writer.close();
     indexSearcher.close();
     System.out.println("done!");
 	}
